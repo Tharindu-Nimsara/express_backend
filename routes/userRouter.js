4329d63b3074c5b53af6a5659d4d5a5e0d33e9e7
Tharindu Nimsara <154465635+Tharindu-Nimsara@users.noreply.github.com>
2025-05-06 15:23:40 +0530

    comment

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,8 +3,8 @@ import { createUser, loginUser } from '../controllers/userControllers.js';
 
 const userRouter = express.Router();
 
-userRouter.post("/",createUser) // localhost:5000/users
-userRouter.post("/login",loginUser) // localhost:5000/users/login
+userRouter.post("/",createUser) // localhost:5000/api/user
+userRouter.post("/login",loginUser) // localhost:5000/api/user/login
 
 
 export default userRouter;
